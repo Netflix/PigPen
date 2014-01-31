60540da436c11617ac3aee16722f88a863f11ace
Matt Bossenbroek <mbossenbroek@netflix.com>
2014-01-31 14:04:23 -0800

    Change output location of unit test

--- src/test/clojure/pigpen/tutorial_test.clj
+++ src/test/clojure/pigpen/tutorial_test.clj
@@ -1,13 +1,14 @@
 (ns pigpen.tutorial-test)
 
+(.mkdirs (java.io.File. "build/tutorial-test"))
 (ns pigpen-demo.core)
 
 (require '[pigpen.core :as pig])
 
 (defn my-data []
-  (pig/load-tsv "input.tsv"))
+  (pig/load-tsv "build/tutorial-test/input.tsv"))
 
-(spit "input.tsv" "1\t2\tfoo\n4\t5\tbar")
+(spit "build/tutorial-test/input.tsv" "1\t2\tfoo\n4\t5\tbar")
 
 (pig/dump (my-data))
 
@@ -55,7 +56,7 @@
     (is (= (pig/dump (my-func data))
            [{:sum 3, :name "foo"}]))))
 
-(pig/write-script "my-script.pig" (my-query "input.tsv" "output.clj"))
+(pig/write-script "build/tutorial-test/my-script.pig" (my-query "input.tsv" "output.clj"))
 
 (deftest test-join
   (let [left  (pig/return [{:a 1 :b 2} {:a 1 :b 3} {:a 2 :b 4}])
