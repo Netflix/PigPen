4844a6467a206f52206d839a2833d53088896270
Matt Bossenbroek <mbossenbroek@netflix.com>
2014-11-01 14:52:18 -0700

    Make functional tests a library

---
+++ pigpen-pig/src/test/clojure/pigpen/pig/functional_test.clj
@@ -0,0 +1,44 @@
+;;
+;;
+;;  Copyright 2013 Netflix, Inc.
+;;
+;;     Licensed under the Apache License, Version 2.0 (the "License");
+;;     you may not use this file except in compliance with the License.
+;;     You may obtain a copy of the License at
+;;
+;;         http://www.apache.org/licenses/LICENSE-2.0
+;;
+;;     Unless required by applicable law or agreed to in writing, software
+;;     distributed under the License is distributed on an "AS IS" BASIS,
+;;     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;     See the License for the specific language governing permissions and
+;;     limitations under the License.
+;;
+;;
+
+(ns pigpen.pig.functional-test
+  (:require [clojure.test :refer [run-tests]]
+            [pigpen.functional-test :as t :refer [TestHarness]]
+            [pigpen.functional-suite :refer [def-functional-tests]]
+            [pigpen.core :as pig]
+            [pigpen.pig :as pig-pig]))
+
+(def prefix "build/functional/pig-rx/")
+
+(.mkdirs (java.io.File. prefix))
+
+(def-functional-tests "pig-rx"
+  (reify TestHarness
+    (data [this data]
+      (pig/return data))
+    (dump [this command]
+      (pig-pig/dump command))
+    (file [this]
+      (str prefix (gensym)))
+    (read [this file]
+      (clojure.string/split-lines
+        (slurp file)))
+    (write [this lines]
+      (let [file (t/file this)]
+        (spit file (clojure.string/join "\n" lines))
+        file))))
---
+++ pigpen-tests/src/main/clojure/pigpen/functional/map_test.clj
@@ -0,0 +1,138 @@
+;;
+;;
+;;  Copyright 2013 Netflix, Inc.
+;;
+;;     Licensed under the Apache License, Version 2.0 (the "License");
+;;     you may not use this file except in compliance with the License.
+;;     You may obtain a copy of the License at
+;;
+;;         http://www.apache.org/licenses/LICENSE-2.0
+;;
+;;     Unless required by applicable law or agreed to in writing, software
+;;     distributed under the License is distributed on an "AS IS" BASIS,
+;;     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;     See the License for the specific language governing permissions and
+;;     limitations under the License.
+;;
+;;
+
+(ns pigpen.functional.map-test
+  (:require [pigpen.functional-test :as t]
+            [pigpen.extensions.test :refer [test-diff]]
+            [pigpen.core :as pig]
+            [pigpen.fold :as fold]))
+
+(t/deftest test-map
+  "normal map"
+  [harness]
+  (test-diff
+    (->>
+      (t/data harness [{:x 1, :y 2}
+                       {:x 2, :y 4}])
+      (pig/map (fn [{:keys [x y]}] (+ x y)))
+      (t/dump harness))
+    '[3 6]))
+
+(t/deftest test-mapcat
+  "normal mapcat"
+  [harness]
+  (test-diff
+    (->>
+      (t/data harness [{:x 1, :y 2}
+                       {:x 2, :y 4}])
+      (pig/mapcat (juxt :x :y))
+      (t/dump harness))
+    '[1 2 2 4]))
+
+(t/deftest test-map-indexed
+  "normal map-indexed"
+  [harness]
+  (test-diff
+    (->>
+      (t/data harness [{:a 2} {:a 1} {:a 3}])
+      (pig/map-indexed vector)
+      (t/dump harness))
+    '[[0 {:a 2}] [1 {:a 1}] [2 {:a 3}]]))
+
+(t/deftest test-map-indexed+sort
+  "sort + map-indexed test"
+  [harness]
+  (test-diff
+    (->>
+      (t/data harness [{:a 2} {:a 1} {:a 3}])
+      (pig/sort-by :a)
+      (pig/map-indexed vector)
+      (t/dump harness))
+    '[[0 {:a 1}] [1 {:a 2}] [2 {:a 3}]]))
+
+(t/deftest test-sort
+  "normal sort"
+  [harness]
+  (test-diff
+    (->>
+      (t/data harness [2 1 4 3])
+      (pig/sort)
+      (t/dump harness))
+    '[1 2 3 4]))
+
+(t/deftest test-sort-desc
+  "descending sort"
+  [harness]
+  (test-diff
+    (->>
+      (t/data harness [2 1 4 3])
+      (pig/sort :desc)
+      (t/dump harness))
+    '[4 3 2 1]))
+
+(t/deftest test-sort-by
+  "normal sort-by"
+  [harness]
+  (test-diff
+    (->>
+      (t/data harness [{:a 2} {:a 1} {:a 3}])
+      (pig/sort-by :a)
+      (t/dump harness))
+    '[{:a 1} {:a 2} {:a 3}]))
+
+(t/deftest test-sort-by-desc
+  "descending sort-by"
+  [harness]
+  (test-diff
+    (->>
+      (t/data harness [{:a 2} {:a 1} {:a 3}])
+      (pig/sort-by :a :desc)
+      (t/dump harness))
+    '[{:a 3} {:a 2} {:a 1}]))
+
+(t/deftest test-sort-by-with-duplicates
+  "sort-by with duplicate input values"
+  [harness]
+  (test-diff
+    (->>
+      (t/data harness [1 2 3 1 2 3 1 2 3])
+      (pig/sort-by identity)
+      (t/dump harness))
+    [1 1 1 2 2 2 3 3 3]))
+
+(t/deftest test-map+fold1
+  "map followed by a fold with a filter"
+  [harness]
+  (test-diff
+    (->>
+      (t/data harness [-2 -1 0 1 2])
+      (pig/map inc)
+      (pig/fold (->> (fold/filter #(> % 0)) (fold/count)))
+      (t/dump harness))
+    [3]))
+
+(t/deftest test-map+fold2
+  "map to boolean followed by a fold with a filter"
+  [harness]
+  (test-diff
+    (->>
+      (t/data harness [-2 -1 0 1 2])
+      (pig/map #(> % 0))
+      (pig/fold (->> (fold/filter identity) (fold/count)))
+      (t/dump harness))
+    [2]))
